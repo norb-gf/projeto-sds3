25189c8ab0f61f52a2968d8fa3f3f3957f8934d0
norb-gf <norb_gf@yahoo.com.br>
2021-05-03 16:21:26 -0300

    Graficos Estaticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,28 @@
 import NavBar from "components/NavBar";
 import Footer from "components/Footer";
 import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
 
 function App() {
   return (
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Ok....</h1>
+        <h1 className="text-primary py-3">DashBoard de Vendas</h1>
+        <div className="row px-3">
+            <div className="col-sm-6 text-secundary text-center">
+              <h5>Taxa de Sucesso</h5>
+              <BarChart />
+            </div>
+            <div className="col-sm-6 text-secundary text-center">
+              <h5>Todas Vendas</h5>
+              <DonutChart />
+            </div>
+        </div>
+        <div className="row py-3">
+          <h2 className="text-primary">Todas Vendas</h2>
+        </ div>
         <DataTable />
       </div>
       <Footer />
